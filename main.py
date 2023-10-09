b88409fff3eb165f632ea489ef8b0932d9cfe517
zzzlook <128834375+zzzlook@users.noreply.github.com>
2023-10-09 14:38:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
-city1 = os.environ['CT']
+# city1 = os.environ['CT']
 city2 = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 
